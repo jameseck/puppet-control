052376c7333ab2ab93d2534e38b68ee4edb84cd6
James Eckersall <james.eckersall@gmail.com>
2018-06-02 19:07:18 +0100

    eyaml gem

--- bootstrap_r10k.sh
+++ bootstrap_r10k.sh
@@ -2,7 +2,6 @@
 set -e
 
 mkdir -p /tmp/r10k_boot/{modules,r10k}
-touch /tmp/r10k_boot/r10k/hiera.yaml
 puppet module install \
   --target-dir=/tmp/r10k_boot/modules \
   puppet-r10k
@@ -13,7 +12,7 @@ puppet module install \
 puppet apply \
   --confdir=/tmp/r10k_boot/r10k \
   --modulepath=/tmp/r10k_boot/modules \
-  --hiera_config=/tmp/r10k_boot/r10k/hiera.yaml \
+  --hiera_config=/etc/puppetlabs/puppet/hiera.yaml \
   --codedir=/tmp/r10k_boot/r10k \
   configure_r10k.pp
 echo "success"
