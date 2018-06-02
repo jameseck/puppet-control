3658cbd41bb48409393c2b8e01866404379ce217
James Eckersall <james.eckersall@gmail.com>
2018-06-02 19:32:44 +0100

    r10k bootstrap

--- configure_r10k.pp
+++ configure_r10k.pp
@@ -10,7 +10,7 @@ package { 'hiera-eyaml-puppetserver':
 }
 class { 'r10k':
   remote          => 'https://github.com/jameseck/puppet-control.git',
-  basedir         => '/etc/puppetlabs/code/environments',
+  r10k_basedir    => '/etc/puppetlabs/code/environments',
   provider        => 'puppet_gem',
   deploy_settings => {
     'purge_levels' => [ 'deployment', 'environment', 'puppetfile' ],
