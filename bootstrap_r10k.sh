dfbf1c5b8e91c0251b2551d6a1fa2f0c04c54b36
James Eckersall <james.eckersall@gmail.com>
2018-06-02 18:54:25 +0100

    fix bugs with r10k bootstrap

--- bootstrap_r10k.sh
+++ bootstrap_r10k.sh
@@ -3,7 +3,14 @@ set -e
 
 mkdir -p /tmp/r10k_boot/{modules,r10k}
 touch /tmp/r10k_boot/r10k/hiera.yaml
-puppet module install puppet-r10k --target-dir=/tmp/r10k_boot/modules
-puppet apply --confdir=/tmp/r10k_boot/r10k --modulepath=/tmp/r10k_boot/modules --hiera_config=/tmp/r10k_boot/r10k/hiera.yaml configure_
-r10k.pp  --codedir=/tmp/r10k_boot/r10k
+puppet module install \
+  --target-dir=/tmp/r10k_boot/modules
+  puppet-r10k
+
+puppet apply \
+  --confdir=/tmp/r10k_boot/r10k \
+  --modulepath=/tmp/r10k_boot/modules \
+  --hiera_config=/tmp/r10k_boot/r10k/hiera.yaml \
+  --codedir=/tmp/r10k_boot/r10k \
+  configure_r10k.pp
 echo "success"
