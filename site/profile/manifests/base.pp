542149cf75486ef6ea39682c243826742233956f
James Eckersall <james.eckersall@gmail.com>
2018-06-05 09:57:37 +0100

    added user

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -13,4 +13,22 @@ class profile::base (
     enable => true,
   }
 
+  group { 'james':
+    ensure => present,
+    gid    => '1000',
+  }
+  user { 'james':
+    ensure   => present,
+    uid      => '1000',
+    gid      => '1000',
+    password => lookup('james_user_password'),
+  }
+
+  ssh_authorized_key { 'james':
+    ensure => present,
+    user   => 'james',
+    type   => 'ssh-rsa',
+    key    => lookup('james_user_ssh_pub_key'),
+  }
+
 }
