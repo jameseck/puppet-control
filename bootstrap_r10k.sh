39053396877a0ea867618483acd719fd7a944fba
James Eckersall <james.eckersall@gmail.com>
2018-06-13 15:05:15 +0100

    fix for bootstrap r10k

--- bootstrap_r10k.sh
+++ bootstrap_r10k.sh
@@ -2,12 +2,15 @@
 set -e
 
 mkdir -p /tmp/r10k_boot/{modules,r10k}
-puppet module install \
-  --target-dir=/tmp/r10k_boot/modules \
-  puppet-r10k
-puppet module install \
-  --target-dir=/tmp/r10k_boot/modules \
-  puppetlabs-puppetserver_gem
+
+MODULES="puppet-r10k puppetlabs/stdlib puppetlabs/ruby puppetlabs/gcc puppet/make puppetlabs/inifile puppetlabs/vcsrepo puppetlabs/git gentoo/portage puppetlabs-puppetserver_gem"
+
+for m in $MODULES; do
+  puppet module install \
+    --target-dir=/tmp/r10k_boot/modules \
+    --modulepath=/tmp/r10k_boot/modules \
+    $m
+done
 
 puppet apply \
   --confdir=/tmp/r10k_boot/r10k \
@@ -15,4 +18,7 @@ puppet apply \
   --hiera_config=/etc/puppetlabs/puppet/hiera.yaml \
   --codedir=/tmp/r10k_boot/r10k \
   configure_r10k.pp
+
 echo "success"
+~
+
