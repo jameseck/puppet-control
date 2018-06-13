9e6b7734564f82319837dbddd0346d54d1009ad9
James Eckersall <james.eckersall@gmail.com>
2018-06-13 12:51:08 +0100

    added r10k

--- site/role/manifests/foreman.pp
+++ site/role/manifests/foreman.pp
@@ -2,7 +2,12 @@ class role::foreman (
 ) {
 
   class { 'r10k':
-    remote => 'https://www.github.com:jameseck/puppet-control.git',
+    remote          => 'https://github.com/jameseck/puppet-control.git',
+    r10k_basedir    => '/etc/puppetlabs/code/environments',
+    provider        => 'puppet_gem',
+    deploy_settings => {
+      'purge_levels' => [ 'deployment', 'environment', 'puppetfile' ],
+    }
   }
 
   class {'r10k::webhook':
@@ -13,4 +18,18 @@ class role::foreman (
     use_mcollective => false,
   }
 
+  package { 'hiera-eyaml-puppet':
+    ensure   => installed,
+    name     => 'hiera-eyaml',
+    provider => 'puppet_gem',
+  }
+  package { 'hiera-eyaml-puppetserver':
+    ensure   => installed,
+    name     => 'hiera-eyaml',
+    provider => 'puppetserver_gem',
+    notify   => Service['puppetserver'],
+  }
+  service { 'puppetserver':
+    ensure => running,
+  }
 }
