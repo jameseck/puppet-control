94d617131920c86a819baa31eb46efbda65b0c65
James Eckersall <james.eckersall@gmail.com>
2018-06-14 12:48:38 +0100

    vhost samba

--- site/role/manifests/vhost.pp
+++ site/role/manifests/vhost.pp
@@ -1,6 +1,11 @@
 class role::vhost (
-  String[1]                                                 $workgroup   = 'workgroup',
-  Array[Struct[{name => String[1], password => String[1]}]] $samba_users = undef,
+  String[1] $workgroup    = 'workgroup',
+  Array[
+    Struct[{
+      name     => String[1],
+      password => String[1],
+    }]]     $samba_users  = undef,
+  Array     $samba_shares = [],
 ) {
 
   class { 'selinux':
@@ -8,16 +13,8 @@ class role::vhost (
     type => 'targeted',
   }
 
-  $shares = [
-    '/export/pool1',
-    '/export/pool1/films',
-    '/export/pool1/music',
-    '/export/pool1/origin',
-    '/export/pool1/pv',
-    '/export/pool1/tv',
-  ]
 
-  $shares.each |$s| {
+  $samba_shares.each |$s| {
     selinux::fcontext { "set-export-fcontext-${s}":
       ensure   => present,
       pathspec => "${s}(/.*)?",
@@ -52,13 +49,7 @@ class role::vhost (
     browsable     => true,
     read_only     => false,
     force_group   => 'root',
-#    force_user    => 'root',
     valid_users   => 'james',
-#    create_mask          => 0644,
-#    force_create_mask    => 0644,
-#    directory_mask       => 0755,
-#    force_directory_mask => 0755,
-#    copy                 => 'some-other-share',
   }
 
 }
