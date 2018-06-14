21eea6831df306a5d51bc73a36ef3afc782eafff
James Eckersall <james.eckersall@gmail.com>
2018-06-14 10:07:37 +0100

    vhost samba

--- site/role/manifests/vhost.pp
+++ site/role/manifests/vhost.pp
@@ -1,5 +1,30 @@
 class role::vhost (
+  String $workgroup = 'jehome',
 ) {
 
+
+  class { 'samba::server':
+    workgroup     => $workgroup,
+    server_string => $facts['fqdn'],
+    interfaces    => 'lo br0',
+    security      => 'user'
+  }
+
+  samba::server::share { 'pool1':
+    comment       => 'pool1',
+    path          => '/export/pool1',
+    guest_only    => false,
+    guest_ok      => false,
+    guest_account => 'guest',
+    browsable     => true,
+    read_only     => false,
+#    create_mask          => 0644,
+#    force_create_mask    => 0644,
+#    directory_mask       => 0755,
+#    force_directory_mask => 0755,
+#    force_group          => 'james',
+#    force_user           => 'james',
+#    copy                 => 'some-other-share',
+  }
 
 }
