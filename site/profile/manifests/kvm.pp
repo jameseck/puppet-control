dd0a90a37d7d4e1f8acf8be4e3102063109229c4
James Eckersall <james.eckersall@gmail.com>
2018-08-12 12:39:24 +0100

    fix grub

--- site/profile/manifests/kvm.pp
+++ site/profile/manifests/kvm.pp
@@ -10,29 +10,31 @@ class profile::kvm (
 #    value  => 'ttyS0',
 #  }
 
-  grub_config { 'GRUB_TERMINAL':
-    value => 'console serial',
+  file_line { 'GRUB_TERMINAL':
+    path  => '/etc/default/grub',
+    match => '^GRUB_TERMINAL=',
+    line  => 'GRUB_TERMINAL="console serial"',
   }
 
-  grub_config { 'GRUB_TERMINAL_OUTPUT':
-    value => 'console serial',
+  -> file_line { 'GRUB_TERMINAL_OUTPUT':
+    path  => '/etc/default/grub',
+    match => '^GRUB_TERMINAL_OUTPUT=',
+    line  => 'console serial',
   }
 
-  file_line { 'GRUB_CMDLINE_LINUX_DEFAULT':
-    path   => '/etc/default/grub',
-    match  => '^GRUB_CMDLINE_LINUX_DEFAULT=',
-    line   => 'GRUB_CMDLINE_LINUX_DEFAULT="console=tty0 console=ttyS0,115200n8"',
-    notify => Exec['grub2-mkconfig'],
+  -> file_line { 'GRUB_CMDLINE_LINUX_DEFAULT':
+    path  => '/etc/default/grub',
+    match => '^GRUB_CMDLINE_LINUX_DEFAULT=',
+    line  => 'GRUB_CMDLINE_LINUX_DEFAULT="console=tty0 console=ttyS0,115200n8"',
   }
 
-  file_line { 'grub serial command':
-    path   => '/etc/default/grub',
-    match  =>  '^GRUB_SERIAL_COMMAND=',
-    line   => 'GRUB_SERIAL_COMMAND="serial --unit=0 --speed=115200 --word=8 --parity=no --stop=1"',
-    notify => Exec['grub2-mkconfig'],
+  -> file_line { 'grub serial command':
+    path  => '/etc/default/grub',
+    match =>  '^GRUB_SERIAL_COMMAND=',
+    line  => 'GRUB_SERIAL_COMMAND="serial --unit=0 --speed=115200 --word=8 --parity=no --stop=1"',
   }
 
-  exec { 'grub2-mkconfig':
+  ~> exec { 'grub2-mkconfig':
     command     => '/usr/sbin/grub2-mkconfig /boot/grub2/grub.cfg',
     refreshonly => true,
   }
